33a9c9532cbd02e6a0519ec3a6886c90d1b49f3c
MirzaRehan53 <mirzarehan708@gmail.com>
2025-03-23 21:55:48 +0500

    initial files added

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import customerReducer from "./features/customer/customerSlice";
+
+import accountReducer from "./features/account/accountSlice";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
+export default store;
